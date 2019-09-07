82afa06c4b610cefa6efa18ec0fac94e1f110db4
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-07 18:42:56 +0900

    Reactを使ったHello world

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-alert('Hello ES2015!');
+// Reactのブラウザとアプリ開発で共通したライブラリ
+import React from 'react';
+// Reactのブラウザに特化したライブラリ
+import ReactDom from 'react-dom';
+
+ReactDom.render(<div>Hello React!!!</div>, document.querySelector('.container'));
