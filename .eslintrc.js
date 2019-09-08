ab0e093d02ba924ec9de512e40b3c28727bc8195
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-08 18:43:53 +0900

    clean app

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,5 @@ module.exports = {
     ],
     "env": {
       "browser" : true
-    },
-    "rules": {
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
     }
 };
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,6 @@
 // Reactのブラウザとアプリ開発で共通したライブラリ
 import React, { Component } from 'react';
 
-import Greeting from './greeting';
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -11,21 +9,9 @@ class App extends Component {
     };
   }
 
-  handleNameChange(name) {
-    // console.log(e.target.value);
-    this.setState({ name });
-  }
-
   render() {
     return (
       <div>
-        <input
-          type="text"
-          value={this.state.name}
-          onChange={event => this.handleNameChange(event.target.value)}
-        />
-        <button onClick={() => this.handleNameChange('Bob')}> I am Bob </button>
-        <Greeting name={this.state.name} />
       </div>
     );
   }
