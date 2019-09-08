18ef104a3776eaccd848ef6c586845eae35f9d69
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-08 11:04:51 +0900

    class component

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,10 @@
 // Reactのブラウザとアプリ開発で共通したライブラリ
-import React from 'react';
+import React, { Component } from 'react';
 
-function App(props) {
-  return (<div>Hello from functional App</div>)
+class App extends Component {
+  render() {
+    return (<div>Hello from class App</div>)
+  }
 }
 
 export default App;
