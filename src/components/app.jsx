5f784261d51bf4ed9daf8816eecff53a5a867d56
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-14 11:21:01 +0900

    APIから緯度経度を取得する

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,60 @@
 // Reactのブラウザとアプリ開発で共通したライブラリ
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+import api from './api';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'John',
     };
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handelPlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, {
+        params: { address: place, key: api.GEOCODE_API },
+      })
+      .then((results) => {
+        const data = results.data;
+        const result = results.data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULTS': {
+            this.setErrorMessage('結果が見つかりませんでした。');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました。');
+            break;
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました。');
+      })
+      ;
   }
 
   render() {
