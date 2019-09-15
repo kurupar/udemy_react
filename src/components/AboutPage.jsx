d32b68de9a4568af44e2a474190600fc2c13419b
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-15 18:20:22 +0900

    React RouterのインストールとAboutページの作成

---
+++ src/components/AboutPage.jsx
@@ -0,0 +1,10 @@
+// Reactのブラウザとアプリ開発で共通したライブラリ
+import React from 'react';
+
+const AboutPage = () => (
+  <div className="about">
+    <h1 className="app-title">このサイトについて</h1>
+  </div>
+);
+
+export default AboutPage;
