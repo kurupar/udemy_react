cf857cfc521d32a21fe2152b3b2759226a906ff7
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-15 20:56:48 +0900

    getパラメータをアプリ内で利用する

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,43 +1,24 @@
 // Reactのブラウザとアプリ開発で共通したライブラリ
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 
-class SearchForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      place: '東京タワー',
-    };
-  }
-
-  handelPlaceChange(place) {
-    this.setState({ place });
-  }
-
-  handelSubmit(e) {
-    // リロードの動きをストップ
-    e.preventDefault();
-    this.props.onSubmit(this.state.place);
-  }
-
-  render() {
-    return (
-      <form className="search-form" onSubmit={e => this.handelSubmit(e)}>
-        <input
-          className="place-input"
-          type="text"
-          size="30"
-          value={this.state.place}
-          onChange={e => this.handelPlaceChange(e.target.value)}
-        />
-        <input className="submit-button" type="submit" value="検索" />
-      </form>
-    );
-  }
-}
+const SearchForm = props => (
+  <form className="search-form" onSubmit={e => props.onSubmit(e)}>
+    <input
+      className="place-input"
+      type="text"
+      size="30"
+      value={props.place}
+      onChange={e => props.onPlaceChange(e.target.value)}
+    />
+    <input className="submit-button" type="submit" value="検索" />
+  </form>
+);
 
 SearchForm.propTypes = {
+  place: PropTypes.string.isRequired,
+  onPlaceChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
 
--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import queryString from 'query-string';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
@@ -16,12 +17,29 @@ class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      place: this.getPlaceParam() || '東京タワー',
       location: {
         lat: 35.6585805,
         lng: 139.7454329,
       },
       sortKey: 'price',
     };
+  }
+
+  componentDidMount() {
+    const place = this.getPlaceParam();
+    if (place) {
+      this.startSearch(place);
+    }
+  }
+
+  getPlaceParam() {
+    const params = queryString.parse(this.props.location.search);
+    const place = params.place;
+    if (place && place.length > 0) {
+      return place;
+    }
+    return null;
   }
 
   setErrorMessage(message) {
@@ -34,9 +52,18 @@ class SearchPage extends Component {
     });
   }
 
-  handelPlaceSubmit(place) {
-    this.props.history.push(`/?query=${place}`);
-    geocode(place)
+  handelPlaceChange(place) {
+    this.setState({ place });
+  }
+
+  handelPlaceSubmit(e) {
+    e.preventDefault();
+    this.props.history.push(`/?place=${this.state.place}`);
+    this.startSearch();
+  }
+
+  startSearch() {
+    geocode(this.state.place)
       .then(({ status, address, location }) => {
         switch (status) {
           case 'OK': {
@@ -73,7 +100,11 @@ class SearchPage extends Component {
     return (
       <div className="search-page">
         <h1 className="app-title">ホテル検索</h1>
-        <SearchForm onSubmit={place => this.handelPlaceSubmit(place)} />
+        <SearchForm
+          place={this.state.place}
+          onPlaceChange={place => this.handelPlaceChange(place)}
+          onSubmit={e => this.handelPlaceSubmit(e)}
+        />
         <div className="result-area">
           <Map location={this.state.location} />
           <div className="result-right">
@@ -96,6 +127,7 @@ class SearchPage extends Component {
 
 SearchPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+  location: PropTypes.shape({ search: PropTypes.string }).isRequired,
 };
 
 export default SearchPage;
