161734b45c82227c8adfe6a334c724dca4839824
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-15 09:38:38 +0900

    表示する情報の追加

--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,3 +1,4 @@
+import geolib from 'geolib';
 import Rakuten from '../lib/Rakuten';
 import Key from '../key/Key';
 
@@ -12,10 +13,20 @@ export const searchHotelByLocation = (location) => {
   return Rakuten.Travel.simpleHotelSearch(params)
     .then(result => result.data.hotels.map((hotel) => {
       const basicInfo = hotel.hotel[0].hotelBasicInfo;
+      const price = basicInfo.hotelMinCharge
+      const distance = geolib.getDistance(
+        { latitude: location.lat, longitude: location.lng },
+        { latitude: basicInfo.latitude, longitude: basicInfo.longitude }
+      );
       return {
         id: basicInfo.hotelNo,
         name: basicInfo.hotelName,
         url: basicInfo.hotelInformationUrl,
+        thumbUrl: basicInfo.hotelThumbnailUrl,
+        price: price ? `${price}円` : '空室なし',
+        reviewAverage: basicInfo.reviewAverage,
+        reviewCount: basicInfo.reviewCount,
+        distance,
       };
     }));
 };
