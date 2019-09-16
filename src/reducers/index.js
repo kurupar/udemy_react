99831455eaecfc0fcfaba56e8fe11108d6670707
kurupar <29848806+kurupar@users.noreply.github.com>
2019-09-16 11:03:25 +0900

    Reducerの分割とAjaxからのdispatch

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,31 @@
-export default (state = { place: 'hoge' }, action) => {
+import { combineReducers } from 'redux';
+
+const place = (state = '東京タワー', action) => {
   switch (action.type) {
     case 'CHANGE_PLACE':
-      // stateは触らない、新しく生成しコピー後に書き換えたいところだけ別にセットする。
-      return Object.assign({}, state, { place: action.place });
+      return action.place;
     default:
       return state;
   }
 };
+
+
+const geocodeResult = (
+  state = {
+    address: '',
+    location: { lat: 35.6585805, lng: 139.7454329 },
+  },
+  action,
+) => {
+  switch (action.type) {
+    case 'GEOCODE_FETCHED':
+      return {
+        address: action.address,
+        location: action.location,
+      };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ place, geocodeResult });
